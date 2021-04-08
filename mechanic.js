a7508a053a76dc3680cd10842c9f32d7786bc83e
SOSHSU <tigerhsu@pacificamerican.org>
2021-04-08 21:20:08 +0800

    music done

--- mechanic.js
+++ mechanic.js
@@ -3,6 +3,17 @@ var pic1 = "http://likea.ezvivi.com/manage/0/product/183274/17582.jpg"
 var deadPic = "https://solutionsreview.com/business-process-management/files/2018/06/Is-BPM-Dead.jpg"
 var survivePic = "https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg"
 
+var song1 = new Audio("audio/Explosion+4.mp3")
+var song2 = new Audio("audio/yt1s.com - Night Sound Effect for sleeping.mp3")
+var song3 = new Audio("audio/Relaxing Forest Nature Sounds-Soothing Birds Singing-Natural Sound of Water Relaxation.mp3")
+var song4 = new Audio("audio/bear-roar.mp3")
+var song5 = new Audio("audio/yt1s.com - VSTRun away Horror music.mp3")
+var song6 = new Audio("audio/yt1s.com - Snowstorm Sound Effect  Winter Storm  Blizzard.mp3")
+var song7 = new Audio("audio/yt1s.com - Beethoven  Symphony No 5 1hour version.mp3")
+var song8 = new Audio("audio/end music.mp3")
+var song9 = new Audio("audio/yt1s.com - Dark Souls  You Died  Sound Effect.mp3")
+var song10 = new Audio("audio/game-over-04-sound-effect-31945801.mp3")
+song1.play()
 display.innerHTML = `<img class = "picture" src = ${pic1}>`
 document.querySelector(".display").classList.toggle("pictureSize")
 
@@ -36,6 +47,8 @@ function reload()
 
 function question1()
 {
+    song1.pause()
+    song2.play()
     document.querySelector(".text").innerText = "After you wake up, you find out the night comes. What do you do?"
     document.querySelector("#startB").classList.toggle("appear")
     var pic2 = "https://upload.wikimedia.org/wikipedia/commons/8/8b/Night_jungle_trek-_looks_un-nerving%21_%2811464532486%29.jpg"
@@ -45,6 +58,8 @@ function question1()
 }
 function dead1()
 {
+    song2.pause()
+    song9.play()
     display.innerHTML = `<img class = "picture" src = ${deadPic}>`
     document.querySelector(".text").innerText = "You die. To make a fire by friction requires a few hours. Before the success, you are already eaten by other animals."
     document.querySelector("#restartB").classList.toggle("appear")
@@ -54,6 +69,8 @@ function dead1()
 
 function question2()
 {
+    song2.pause()
+    song3.play()
     document.querySelector(".text").innerText = "After this night, you decide to move. Which direction are you going to go?"
     var pic3 = "https://blog.scienceborealis.ca/wp-content/uploads/sites/2/2017/06/forest-868715_960_720-640x410.jpeg"
     display.innerHTML = `<img class = "picture" src = ${pic3}>`
@@ -66,6 +83,8 @@ function question2()
 }
 function dead2()
 {
+    song3.pause()
+    song9.play()
     display.innerHTML = `<img class = "picture" src = ${deadPic}>`
     document.querySelector(".text").innerText = "You die. You lost in the forest."
     document.querySelector("#restartB").classList.toggle("appear")
@@ -76,6 +95,9 @@ function dead2()
 
 function question3()
 {
+    song3.pause()
+    song4.play()
+    song5.play()
     document.querySelector(".text").innerText = "Walking for a few hours. Suddenly, you see a bear. It is 5 meters away and it stares at you. What do you do?"
     var pic4 = "https://www.wkar.org/sites/wkar/files/201304/Small%20Bear%20growl%20Clash_1270928.jpg"
     display.innerHTML = `<img class = "picture" src = ${pic4}>`
@@ -89,6 +111,9 @@ function question3()
 }
 function dead3()
 {
+    song4.pause()
+    song5.pause()
+    song9.play()
     display.innerHTML = `<img class = "picture" src = ${deadPic}>`
     document.querySelector(".text").innerText = "You die. Your action stimulated the bear."
     document.querySelector("#restartB").classList.toggle("appear")
@@ -99,6 +124,9 @@ function dead3()
 
 function question4()
 {
+    song4.pause()
+    song5.pause()
+    song6.play()
     document.querySelector(".text").innerText = "Fortunately, You find out some fruits to eat. However, it is snowy and sunset now. It is about -30 degrees Celsius. You see a dead horse. What do you do?"
     var pic5 = "https://m.media-amazon.com/images/I/71lHmxWsJML._AC_SL1024_.jpg"
     display.innerHTML = `<img class = "picture" src = ${pic5}>`
@@ -112,6 +140,8 @@ function question4()
 }
 function dead5()
 {
+    song6.pause()
+    song9.play()
     display.innerHTML = `<img class = "picture" src = ${deadPic}>`
     document.querySelector(".text").innerText = "You froze to death."
     document.querySelector("#restartB").classList.toggle("appear")
@@ -123,6 +153,8 @@ function dead5()
 
 function question5()
 {
+    song6.pause()
+    song7.play()
     document.querySelector(".text").innerText = "After you wake up, the heat from the sun warm you up. You begin to walk. You find out some footprints. What do you do?"
     var pic6 = "https://st.depositphotos.com/3057233/4606/i/950/depositphotos_46064027-stock-photo-footprints-in-the-snow-in.jpg"
     display.innerHTML = `<img class = "picture" src = ${pic6}>`
@@ -136,6 +168,8 @@ function question5()
 
 function dead6()
 {
+    song7.pause()
+    song10.play()
     display.innerHTML = `<img class = "picture" src = ${deadPic}>`
     document.querySelector(".text").innerText = "You decide to ignore it. After few weeks, you can't find any way home and starve to death."
     document.querySelector("#restartB").classList.toggle("appear")
@@ -145,6 +179,8 @@ function dead6()
 }
 function surviveEnd()
 {
+    song7.pause()
+    song8.play()
     display.innerHTML = `<img class = "picture" src = ${survivePic}>`
     document.querySelector(".text").innerText = "Congratulation! You survive and finally go back to your sweet home!"
     document.querySelector("#restartB").classList.toggle("appear")
